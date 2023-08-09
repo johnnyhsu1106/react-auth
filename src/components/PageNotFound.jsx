4208f4e8907728fe3ed44bce93faedb28125ef34
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-08-09 14:10:37 -0500

    use single quote

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -7,7 +7,7 @@ const PageNotFound = () => {
       <h1 class="display-1 fw-bold">404</h1>
       <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
       <p class="lead">
-          The page you’re looking for doesn’t exist.
+          The page you're looking for doesn't exist.
         </p>
       <Link
         className='btn btn-primary' 
