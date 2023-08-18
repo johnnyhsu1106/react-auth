4997b1c15e34e609f33d6977372a6262338c0c45
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-08-18 10:00:04 -0500

    fix issue: use className, not class

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div class="text-center">
-      <h1 class="display-1 fw-bold">404</h1>
-      <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-      <p class="lead">
+    <div className="text-center">
+      <h1 className="display-1 fw-bold">404</h1>
+      <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+      <p className="lead">
           The page you're looking for doesn't exist.
         </p>
       <Link
